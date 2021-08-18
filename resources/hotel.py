482b906b72bdced8cf7ab09469dd60d66a0e2038
GustavoNeery <gustavoalvesneryy@hotmail.com>
2021-08-17 21:24:56 -0300

    busca por atributos especificos do hotel

--- resources/hotel.py
+++ resources/hotel.py
@@ -1,9 +1,63 @@
 from flask_restful import Resource, reqparse
 from models.hotel import HotelModel
 from flask_jwt_extended import jwt_required
+import sqlite3
+
+def normalize_path_params(cidade=None,
+                          estrelas_min = 0,
+                          estrelas_max = 5,
+                          diaria_min=0,
+                          diaria_max=10000,
+                          limite = 50,
+                          offset = 0, **dados):
+    if cidade:
+        return {
+            'estrelas_min': estrelas_min,
+            'estrelas_max': estrelas_max,
+            'diaria_min': diaria_min,
+            'diaria_max': diaria_max,
+            'cidade': cidade,
+            'limit': limit,
+            'offset': offset
+        }
+    return {
+        'estrelas_min': estrelas_min,
+        'estrelas_max': estrelas_max,
+        'diaria_min': diaria_min,
+        'diaria_max': diaria_max,
+        'limit': limit,
+        'offset': offset
+    }
+        
+    
+
+path_params = reqparse.RequestParser()
+path_params.add_argument('cidade', type=str)
+path_params.add_argument('estrelas_min', type=str)
+path_params.add_argument('estrelas_max', type=str)
+path_params.add_argument('diaria_min', type=str)
+path_params.add_argument('diaria_max', type=str)
+path_params.add_argument('limit', type=str)
+path_params.add_argument('offset', type=str)
 
 class Hoteis(Resource):
     def get(self):
+        connection = sqlite3.connect('banco.dc')
+        cursor = connection.cursor()
+
+
+        dados = path_params.parse_args()
+        dados_validos = {chave:dados[chave] for chave in dados if dados[chave] is not None}
+        parametros = normalize_path_params(**dados_validos)
+
+        if parametros.get('cidade'):
+            consulta = "SELECT * FROM hoteis \
+            WHERE (estrelas > ? and estrelas < ? \
+            and (diaria > ? ? and diaria < ? \
+            LIMIT ? OFFSET ?"
+            tupla = tupla([parametros[chave] for chave in parametros])
+            resultado = cursor.excute(consulta, ())
+
         return {'hoteis' : [hotel.json() for hotel in HotelModel.query.all()]}
 
 class Hotel(Resource):
@@ -14,7 +68,7 @@ class Hotel(Resource):
     atributos.add_argument('cidade')
 
 
-
+    
     #lista de um hotel com id especifico
     def get(self, hotel_id):
         hotel = HotelModel.find_hotel(hotel_id)
